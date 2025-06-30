1f835df842cb2b086a6374713a2e00c518766ba5
timan-z <58091424+timan-z@users.noreply.github.com>
2025-06-30 18:04:00 -0400

    Looks like using Colly might not be the best approach here, at least for scraping Letterboxd. The page DOM that you can inspect and see is very different from what's initially loaded on the site (JS etc adds stuff after). Might need PlayWright instead. Or maybe, I can just work with the base-level loaded HTML and re-arrange how I'm traversing and retrieving information.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -15,6 +16,19 @@ func main() {
 	- the AllowedDomains option will tell c what domains you are allowed to scrape.
 	*/
 	c := colly.NewCollector(colly.AllowedDomains("www.letterboxd.com", "letterboxd.com", "https://letterboxd.com"))
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "letterboxd.com/*", // domains that will be affected here.
+		Delay:       2 * time.Second,    // Set a delay between requests to these domains.
+		RandomDelay: 2 * time.Second,    // Addtional random delay.
+	}) // <-- Definitely should add a delay effect so Letterboxd doesn't IP-ban me.
+
+	// Should have these two functions below (just for debugging, lets you know when the crawling starts + if any errors come your way):
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting %s\n", r.URL)
+	})
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Printf("Error while scraping: %s\n", e.Error())
+	})
 
 	// let's define URL variables for two profiles (we'll take this as CLI arguments later, and eventually from an HTML Form):
 	//urlOne = "jacquesrivette_"
@@ -24,31 +38,46 @@ func main() {
 	c.OnHTML("h1.title-3", func(h *colly.HTMLElement) {
 		fmt.Printf("The Letterboxd profile you're crawling over is: %s\n", h.Text)
 	})
+
 	// Here's where I can start working to extract a list of the films + ratings:
 	c.OnHTML("ul.poster-list", func(h *colly.HTMLElement) {
+		h.ForEach("li.poster-container", func(i int, e *colly.HTMLElement) {
+			//fmt.Printf("DEBUG: How many lines? 72 right?\n")
+			fmt.Printf("DEBUG: Currently iterating through <li> #%d\n", i)
 
-		/*h.DOM.find("li.poster-container").Each(func(i int, s * goquery.Selection) {
-		})*/
-		//selection := h.DOM
-		//childNodes := selection.Children().Nodes
-		//fmt.Printf(selection.Text())
-		//fmt.Printf(childNodes.Text())
-		// li.poster-container
-		//selection := h.DOM
-		//childNodes := selection.Children().Nodes
-		//fmt.Printf(selection)
-		//fmt.Printf(childNodes)
+			/* Inside each <li class="poster-container"> element will be two children:
+			[1] - <div> [2] - <p>, you can search into <div> to extract the film name and release year, <p> for the rating...*/
+			liElem := e.DOM
+			childNodes := liElem.Children().Nodes
+
+			html, _ := e.DOM.Html()
+			fmt.Println("Debug: The <li> html is => ", html)
+
+			// Expect that there are exactly two children elements. Need to account for expected DOM, code is fragile in that respect.
+			if len(childNodes) == 2 {
+				//divElem := liElem.Find("div").First()
+				// Latching onto this <div> element, we can just scan through its entire depth and extract what I want (<a> element with data-original-title attr):
+				//aElem := divElem.Find("div > a[data-original-title]").First()
+				//aElem := e.DOM.Find("div > div > a[data-original-title]").First()
+				//filmTitleYear, exists := aElem.Attr("data-original-title") // returns the value and ig a boolean for if it exists or not.
+				/*if exists {
+					fmt.Printf("Value of filmTitleYear %s\n", filmTitleYear)
+				}*/
+
+				divElemOne := liElem.Find("div").First()
+				divElemTwo := divElemOne.Find("div").First()
+				//aElem := divElemTwo.Find("a[data-original-title]").First()
+				aElem := divElemTwo.Find("a[href]").First()
+				filmTitleYear, exists := aElem.Attr("href")
+
+				if exists {
+					fmt.Printf("The value of filmTitleYear %s\n", filmTitleYear)
+				} else {
+					//fmt.Printf("Nah still not here.")
+				}
+			}
+		})
 	})
 
-	// Should have these two functions below (just for debugging, lets you know when the crawling starts + if any errors come your way):
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Printf("Visiting %s\n", r.URL)
-	})
-	c.OnError(func(r *colly.Response, e error) {
-		fmt.Printf("Error while scraping: %s\n", e.Error())
-	})
-	// We will need to tell c what criteria to watch out for...
-	/*c.OnHTML("div.myclass", func(h *colly.HTMLElement) {
-	})*/
 	c.Visit("https://letterboxd.com/jacquesrivette_/films/rated/.5-5/")
 }
